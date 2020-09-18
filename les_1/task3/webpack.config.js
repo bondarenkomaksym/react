31dcab33286b28b862d73efc75cb57f0751e219a
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-18 16:23:41 +0300

    master

--- les_1/task3/webpack.config.js
+++ les_1/task3/webpack.config.js
@@ -1,51 +1,38 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const {
-  CleanWebpackPlugin
-} = require("clean-webpack-plugin");
-const webpack = require("webpack");
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
-  const config = {
-    entry: "./src/index.js",
-    output: {
-      filename: "bundle.js"
+module.exports = {
+  module: {
+    rules: [{
+      test: /.js$/,
+      use: ["babel-loader"]
     },
-    module: {
-      rules: [{
-        test: /.js$/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /.s?css$/,
-        use: [
-          isProduction ?
-            MiniCssExtractPlugin.loader :
-            "style-loader",
-          "css-loader",
-          "sass-loader",
-        ]
-      }
-      ],
-    },
-    plugins: [
-      new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./src/index.html"
-      }),
-    ],
-    devServer: {
-      hot: true
+    {
+      test: /.css$/,
+      use: [
+        isProduction ?
+          MiniCssExtractPlugin.loader :
+          "style-loader",
+        "css-loader"
+      ]
     }
-  };
+    ],
+  },
+  plugins: [
 
-  if (isProduction) {
-    config.plugins.push(new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }));
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
+  devServer: {
+    hot: true
   }
+};
 
-  return config;
+if (isProduction) {
+  config.plugins.push(new MiniCssExtractPlugin({
+    filename: "[name].css",
+  }));
+}
+
+return config;
 };
