c21370fc89a91fa1243da2c978ae9f78dde96778
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-21 17:22:21 +0300

    master

--- les_3/task3/src/UserInfo.jsx
+++ les_3/task3/src/UserInfo.jsx
@@ -5,9 +5,9 @@ const UserInfo = (props) => {
   return (
     <div className="user-info">
       <Avatar
-        avatarUrl={props.avatarUrl}
-        name={props.name} />
-      <div className="user-info__name">{props.name}</div>
+        avatarUrl={props.user.avatarUrl}
+        name={props.user.name} />
+      <div className="user-info__name">{props.user.name}</div>
     </div>
   );
 };
