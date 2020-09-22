2803f39525ebedf7fefd1282a46bd0472b170ec9
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-22 14:13:17 +0300

    master

--- les_3/task4/src/App.jsx
+++ les_3/task4/src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Greeting from "./Greeting.jsx";
-import moment from "moment";
 
-const age = moment(new Date())
-  .diff(moment(new Date('2003-01-01T11:11:11.819Z')), 'years');
 
 const App = () => {
   return (
     <Greeting
       firstName="John"
       lastName="Doe"
-      birthDate={age}
+      birthDate={(new Date('2003-01-01T11:11:11.819Z'))}
     />
   );
 }
--- les_3/task4/src/Greeting.jsx
+++ les_3/task4/src/Greeting.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import moment from "moment";
 
 const Greeting = (props) => {
+  const age = moment(new Date()).diff(moment(props.birthDate), 'years');
   return (
     <div className="greeting">
       {`My name is ${props.firstName} ${props.lastName}. 
-      I'm ${props.birthDate} years old`}
+      I'm ${age} years old`}
     </div>
   );
 };
