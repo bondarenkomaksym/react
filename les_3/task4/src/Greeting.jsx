ef29f5cee688d94c304e725568ade46b6076ef20
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-22 15:37:24 +0300

    master

--- les_3/task4/src/Greeting.jsx
+++ les_3/task4/src/Greeting.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import moment from "moment";
+// import moment from "moment";
 
 const Greeting = (props) => {
   // console.log(props);
-  const age = moment().diff((props.birthDate), 'years');
+  // const age = moment().diff((props.birthDate), 'years');
+  const age = Math.floor((new Date() - new Date(props.birthDate)) / 1000 / 60 / 60 / 24 / 365.25);
   return (
     <div className="greeting">
       {`My name is ${props.firstName} ${props.lastName}. 
