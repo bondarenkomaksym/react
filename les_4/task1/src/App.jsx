21165f807433d8df054beb52ba2e604e20982bd6
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-22 18:19:23 +0300

    a

--- les_4/task1/src/App.jsx
+++ les_4/task1/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <Counter start={1} interval={1000} />
-      <Counter start={15} interval={1000} />
+      <Counter start={1} interval={1000} />
       <Counter start={-50} interval={100} />
     </>
   );
