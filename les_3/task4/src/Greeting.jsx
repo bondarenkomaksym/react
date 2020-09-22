06e897185160bdf5344575a203eea1c33efc9821
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-22 15:39:02 +0300

    master

--- les_3/task4/src/Greeting.jsx
+++ les_3/task4/src/Greeting.jsx
@@ -7,8 +7,7 @@ const Greeting = (props) => {
   const age = Math.floor((new Date() - new Date(props.birthDate)) / 1000 / 60 / 60 / 24 / 365.25);
   return (
     <div className="greeting">
-      {`My name is ${props.firstName} ${props.lastName}. 
-      I'm ${age} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}
     </div>
   );
 };
