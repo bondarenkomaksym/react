00a3f8c0f1cac269195fc847ea4d30e0ef7646a2
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-23 16:18:19 +0300

    a

---
+++ les_5/task2/src/GoodButton.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+//чтобы не экспортировать библиотеку, можно добавить перед компонентой "React"
+// import React, { Component } from 'react';
+
+class GoodButton extends React.Component {
+  //объявляем обработчик в виде метода
+  handleClick(e) {
+    //в консоль выводим объект события(е)
+    console.log(e.target.textContent);
+    alert("Good job!");
+  }
+  //для лучшей читаемости JSX обработчик(handleClick) выносим наружу
+  render() {
+    return (
+      <button className="fancy-button"
+        onClick={this.handleClick}
+      >
+        Click me!
+      </button>
+    )
+  }
+}
+
+
+// const GoodButton = () => (
+//   <button className="fancy-button"
+//     onClick={() => alert("Good job!")}
+//   >
+//     Click me!
+//   </button>
+// );
+
+export default GoodButton;
+
+
