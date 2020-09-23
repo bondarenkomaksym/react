59018d60cd613802952db286943fb396f7925512
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-23 09:21:42 +0300

    master

---
+++ les_4/task2/src/Clock.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./counter.scss";
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+}
+
+export default getTimeWithOffset;
+
+
