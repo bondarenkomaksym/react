5d8ec87c9a9e8ecc734a8e2fbc56f6a0d42d1aa3
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-26 17:17:54 +0300

    a

--- les_6/task1/src/Auth.jsx
+++ les_6/task1/src/Auth.jsx
@@ -4,32 +4,7 @@ import Login from './Login';
 import Logout from './Logout';
 
 class Auth extends React.Component {
-  state = {
-    isLoggedIn: false
-  }
 
-  handleLogin = () => {
-    this.setState({
-      isLoggedIn: true
-    })
-  }
-
-  handleLogout = () => {
-    this.setState({
-      isLoggedIn: false
-    })
-  }
-
-  render() {
-    return (
-      <div className="panel">
-        <Greeting isLoggedIn={this.state.isLoggedIn} />
-        {!this.state.isLoggedIn
-          ? <Login onLogin={this.handleLogin} />
-          : <Logout onLogout={this.handleLogout} />}
-      </div>
-    )
-  }
 }
 
 export default Auth;
