abbc1497ff691ae3633a9a15b9edc4ba70b92e63
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-27 15:02:28 +0300

    a

---
+++ les_6/task4/src/Offline.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Offline = () =>
+  <div className="status">
+    <span className="status__text">Offline</span>
+    <button className="status__btn">Reconnect</button>
+  </div>
+export default Offline;
