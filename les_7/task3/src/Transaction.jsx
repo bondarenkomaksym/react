30c3ef20468e9ba48dd9c7295c9b392e6c4bf478
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-29 16:10:10 +0300

    a

--- les_7/task3/src/Transaction.jsx
+++ les_7/task3/src/Transaction.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import moment from "moment";
+
+const getDate = moment().format("DD MMM");
 
 const Transaction = ({ from, to, amount, rate, time }) => {
+
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const formatTime = new Date(time);
+  const getTime = new Intl.DateTimeFormat("en-GB", options).format(formatTime);
+  const getAmount = new Intl.NumberFormat("en-GB").format(amount);
+
   return (
     <li className="transaction">
-      <span className="transaction__date"></span>
-      <span className="transaction__time">{time}</span>
-      <span className="transaction__assets">{from}→{to}</span>
+      <span className="transaction__date">{getDate}</span>
+      <span className="transaction__time">{getTime}</span>
+      <span className="transaction__assets">{from} → {to}</span>
       <span className="transaction__rate">{rate}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__amount">{getAmount}</span>
     </li>
   )
 }
