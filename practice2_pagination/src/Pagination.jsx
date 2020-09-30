549a2760568b8be85bb6508b881e48cfdbe82ee6
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-09-30 18:06:19 +0300

    Pagination

--- practice2_pagination/src/Pagination.jsx
+++ practice2_pagination/src/Pagination.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 const Pagination = () => {
 
+  return (
+
+    <div className="pagination">
+      <button className="btn">←</button>
+      <span className="pagination__page">1</span>
+      <button className="btn">→</button>
+    </div>
+  )
 
 };
 
--- practice2_pagination/src/UsersList.jsx
+++ practice2_pagination/src/UsersList.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import Pagination from './Pagination';
+import User from './User';
+
+
+//1. create state - currPage, totalItems +++
+//2. goPrev goNext
+//3. create test data +++
+//4. display data +++
+//5. calculate users to display+++
+
 
 class UsersList extends React.Component {
 
+  state = {
+    currentPage: 0,
+    itemsPerPage: 3
+  };
+
+  goPrev = () => {
+
+
+  }
+
+  goNext = () => {
+
+
+  }
+
   render() {
+
+    //принимаем список поль-лей
+    const users = this.props.users;
+
+    //высчитываем кол поль-лей
+    const currentPage = this.state.currentPage;
+
+    const startIndex = currentPage * 3;
+    const endIndex = startIndex + 3;
+    //через slice режем начальный массив поль-лей
+    const usersToDisplay = users.slice(startIndex, endIndex);
 
     return (
       <div>
-        {/* finish this method */}
+        <Pagination />
+        <ul className="users">
+          {usersToDisplay.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
       </div>
     );
   }
