3b3678a6e24a3d13b0837d8debe2f287bdcdcf5c
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-10-03 14:58:23 +0300

    ConnectionStatus

---
+++ les_8/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+class ConnectionStatus extends React.Component {
+  state = {
+    width: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+
+    const { innerWidth, innerHeight } = window;
+    //установка заголовка по умолчанию
+    this.setDemensions(innerWidth, innerHeight);
+  }
+  //отписка от события
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+  //после изменения размера обновляем состояние state
+  onResize = (e) => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDemensions(innerWidth, innerHeight);
+  }
+
+  //чтобы не было дублирования кода
+  setDemensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${innerWidth} x ${innerHeight}`;
+  };
+
+
+  render() {
+    return (
+      <div className="dimensions">
+        {`${innerWidth}px - ${innerHeight}px`}
+      </div>
+    )
+  }
+}
+
+export default ConnectionStatus;
