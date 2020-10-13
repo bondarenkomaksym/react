f2ecd9d217465ac88b7d2d183eacb2af54ae4618
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-10-13 21:27:25 +0300

    world time with hooks

--- les_14/task5/src/Clock.jsx
+++ les_14/task5/src/Clock.jsx
@@ -10,25 +10,22 @@ function Clock(props) {
   const formatDate = (offset) => moment().utcOffset(offset).format('HH:mm:ss');
   // console.log(formatDate(offset));
 
-  const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState(formatDate(offset + 1));
 
 
   //заменяем componentDidMount и componentWillUnmount
   useEffect(() => {
-    let timerId = setInterval(() => newTime(), 1000);
+    let timerId = setInterval(() => {
+      setTime(formatDate(offset + 1))
+    }, 1000);
 
     return () => clearInterval(timerId);
-
   });
-
-  function newTime() {
-    setTime(new Date());
-  }
 
   return (
     <div className="clock">
       <div className="clock__location">{location}</div>
-      <div className="clock__time">{formatDate(offset)}</div>
+      <div className="clock__time">{time}</div>
     </div>
   );
 }
