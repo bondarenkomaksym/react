dc0b760adfed03bb47cd8be8eee59fce6eb67e84
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-10-15 11:59:38 +0300

    router lifecycle

--- practice3_router/src/User.jsx
+++ practice3_router/src/User.jsx
@@ -1,38 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-const User = () => {
-  const [userData, setUserData] = useState(null);
-  const { userId } = useParams();
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${userId}`)
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error();
-      })
-      .then(userData => {
-        setUserData(userData);
-      });
-  }, [userId]);
-
-  if (!userData) {
-    return null;
+class User extends React.Component {
+  state = {
+    userData: null,
   }
 
-  const { name, location, avatar_url } = userData;
+  componentDidMount() {
+    // debugger;
+    fetch(`https://api.github.com/users/${this.props.match.params.userId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(userData => {
+        // debugger;
+        this.setState({
+          userData: userData,
+        })
+      })
+  }
 
-  return (
-    <div className="user">
-      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-      <div className="user__info">
-        <span className="user__name">{name}</span>
-        <span className="user__location">{location}</span>
+  componentDidUpdate(prevState) {
+    if (this.state.userData !== prevState.userData) {
+      fetch(`https://api.github.com/users/${this.props.match.params.userId}`)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(userData => {
+          this.setState({
+            userData: userData,
+          })
+        })
+    }
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    if (!this.state.userData) {
+      return null;
+    }
+    // debugger;
+    const { name, location, avatar_url } = this.state.userData;
+
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 
